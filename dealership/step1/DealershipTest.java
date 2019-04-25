4383839d032195f49475df018d5a3de417e3c0b9
Undack <Hamish@sturley.org>
2019-04-25 20:22:50 +0100

    Add files via upload

--- dealership/step1/DealershipTest.java
+++ dealership/step1/DealershipTest.java
@@ -1,3 +1,6 @@
+//Authored by B00336796 and B00335847
+//Github at https://github.com/Undack/Coursework/tree/master/dealership
+
 import java.util.ArrayList;
 
 public class DealershipTest {
@@ -5,6 +8,7 @@ public class DealershipTest {
 		Dealership dealership = new Dealership();
 		
 		String input;
+		ArrayList<Object> result;
 		boolean exit = false;
 		while(exit == false) {
 			System.out.println("Welcome to the stock control interface, please select an option: ");
@@ -20,25 +24,30 @@ public class DealershipTest {
 			    break;
 			  case "1":
 				  input = Input.getString("Please enter make of car you would like to search for\n");
-				  	ArrayList<Object> result = dealership.searchMake(input);
-				  	if (!result.isEmpty()) {
-				  		System.out.println("The following Makes matched your query:");
-				  		System.out.println(result);
-				  	} else {
-				  		System.out.println("Unable to find Make in stock");
-				  	}
-				    break;
+				  result = dealership.searchMake(input);
+				  if (!result.isEmpty()) {
+					  System.out.println("The following Makes matched your query:");
+					  System.out.println(result);
+				  } else {
+					  System.out.println("Unable to find Make in stock");
+				  }
+				   break;
 			  case "2":
 				  input = Input.getString("Please enter make of car you would like to remove from stock\n");
-				    if(dealership.removeMake(input)) {
-				    	System.out.println("Make successfully removed");
-				    } else {
-				    	System.out.println("Make was not removed, it may not be present in stock");
-				    }
-				    break;
+				  if(dealership.removeMake(input)) {
+					  System.out.println("Make successfully removed");
+				  } else {
+					  System.out.println("Make was not removed, it may not be present in stock");
+				  }
+				  break;
 			  case "3":
-				  System.out.println("All makes currently stocked:");
-				  System.out.println(dealership.searchMake("*"));
+				  result = dealership.searchMake("*");
+				  if(!result.isEmpty()) {
+					  System.out.println("The following makes were found");
+					  System.out.println(result);
+				  } else {
+					  System.out.println("No makes were found");
+				  }
 				  break;
 			  case "4":
 				  input = Input.getString("Sure you want to exit? (Y/N)\n");
